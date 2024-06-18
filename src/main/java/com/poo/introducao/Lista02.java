a9cbe605a64b35ee579ff885cf7be9d5981f57e2
Yuri Albuquerque Silva <yuri.albuquerque.2@hotmail.com>
2024-06-18 19:57:30 -0300

    Exercício 2 da lista 2 resolvido e ajustes no exercício 1

--- src/main/java/com/poo/introducao/Lista02.java
+++ src/main/java/com/poo/introducao/Lista02.java
@@ -6,8 +6,8 @@ public class Lista02 {
     void ex1(){
         int numDigitado;
 
-        System.out.println("-----------------------------------");
-        System.out.print("Digite um número para saber se é par ou ímpar: ");
+        System.out.println("-----------------------------------------------------");
+        System.out.print("Digite um número para saber se é par, ímpar ou zero: ");
         Scanner sc = new Scanner(System.in);
 
         numDigitado = sc.nextInt();
@@ -21,5 +21,31 @@ public class Lista02 {
         } else{
             System.out.println("O número é 0 (zero).");
         }
+        sc.close();
     }
-}
+
+    void ex2(){
+        double num1;
+        double num2;
+        System.out.println("-------------------------------");
+        System.out.println("Descubra qual o maior número");
+        System.out.println("-------------------------------");
+        System.out.print("Digite um número: ");
+        Scanner sc = new Scanner(System.in);
+
+        num1 = sc.nextDouble();
+
+        System.out.print("Digite o último número: ");
+
+        num2 = sc.nextDouble();
+        System.out.println("-------------------------------");
+
+        if(num1 > num2){
+            System.out.println("O número " + num1 + " é maior que " + num2);
+        } else {
+            System.out.println("O número " + num2 + " é maior que " + num1);
+        }
+        System.out.println("-------------------------------");
+        sc.close();
+    }
+    }
