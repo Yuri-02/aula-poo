2c77dbac95377323ae281b79b020480cd87770ee
Yuri Albuquerque Silva <yuri.albuquerque.2@hotmail.com>
2024-06-18 21:03:26 -0300

    Exercício 3 da lista 2 resolvido

--- src/main/java/com/poo/introducao/Lista02.java
+++ src/main/java/com/poo/introducao/Lista02.java
@@ -1,9 +1,10 @@
 package com.poo.introducao;
+
 import java.util.Scanner;
 
 public class Lista02 {
 
-    void ex1(){
+    void ex1() {
         int numDigitado;
 
         System.out.println("-----------------------------------------------------");
@@ -12,19 +13,19 @@ public class Lista02 {
 
         numDigitado = sc.nextInt();
 
-        if (numDigitado != 0){
-            if((numDigitado % 2) == 0){
+        if (numDigitado != 0) {
+            if ((numDigitado % 2) == 0) {
                 System.out.println("O número " + numDigitado + " é par.");
             } else {
                 System.out.println("O número " + numDigitado + " é ímpar.");
             }
-        } else{
+        } else {
             System.out.println("O número é 0 (zero).");
         }
         sc.close();
     }
 
-    void ex2(){
+    void ex2() {
         double num1;
         double num2;
         System.out.println("-------------------------------");
@@ -40,7 +41,7 @@ public class Lista02 {
         num2 = sc.nextDouble();
         System.out.println("-------------------------------");
 
-        if(num1 > num2){
+        if (num1 > num2) {
             System.out.println("O número " + num1 + " é maior que " + num2);
         } else {
             System.out.println("O número " + num2 + " é maior que " + num1);
@@ -48,4 +49,37 @@ public class Lista02 {
         System.out.println("-------------------------------");
         sc.close();
     }
+
+    void ex3() {
+        String idoso;
+        String gestante;
+        String deficiente;
+
+        System.out.println("-------------------");
+        System.out.println("Atendimento virtual");
+        System.out.println("-------------------");
+        System.out.print("Você tem 65 anos ou mais? Responda com s/n: ");
+        Scanner sc = new Scanner(System.in);
+
+        idoso = sc.nextLine();
+
+        if (idoso.equals("s")) {
+            System.out.println("Encaminhe-se à fila preferencial.");
+        } else {
+            System.out.print("Você é gestante? Responda com s/n: ");
+            gestante = sc.nextLine();
+            if (gestante.equals("s")) {
+                System.out.println("Encaminhe-se à fila preferencial.");
+            } else {
+                System.out.print("Você é deficiente? Responda com s/n: ");
+                deficiente = sc.nextLine();
+                if (deficiente.equals("s")) {
+                    System.out.println("Encaminhe-se à fila preferencial.");
+                } else {
+                    System.out.println("Encaminh-se à fila comum.");
+                }
+            }
+        }
+        sc.close();
     }
+}
