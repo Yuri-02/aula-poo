63f1dc37406f5dc51ac0951d309665b0e085c3c9
Yuri Albuquerque Silva <yuri.albuquerque.2@hotmail.com>
2024-06-20 22:10:22 -0300

    Exercício 3 resolvido

--- src/main/java/com/poo/introducao/Lista03.java
+++ src/main/java/com/poo/introducao/Lista03.java
@@ -1,8 +1,8 @@
 package com.poo.introducao;
 
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
+//import java.util.Timer;
+//import java.util.TimerTask;
 
 public class Lista03 {
     
@@ -58,6 +58,50 @@ public class Lista03 {
 
         for (int i=0; i<=10; i++){
             System.out.println(num + " x " + i + " = " + num*i);
+        }
+        sc.close();
+    }
+
+    void ex3(){
+        int num1;
+        int num2;
+        int i = 0;
+        int pares=0;
+
+        System.out.println("--------------------------------");
+        System.out.println("Números pares entre dois valores");
+        System.out.println("--------------------------------");
+        System.out.print("Digite um número inteiro: ");
+        Scanner sc = new Scanner(System.in);
+
+        num1 = sc.nextInt();
+
+        System.out.print("Digite outro número inteiro: ");
+        
+        num2 = sc.nextInt();
+
+        if(num2 > num1){
+            while(pares < num2){
+                if((num1 % 2)==0){
+                    System.out.println(pares = num1 + i);
+                    i +=2;
+                } else {
+                    System.out.println(pares = num1 + i + 1);
+                    i++;
+                }
+                
+            }
+        } else {
+            while(pares < num1){
+                if((num1 % 2)==0){
+                    System.out.println(pares = num1 + i);
+                    i +=2;
+                } else {
+                    System.out.println(pares = num1 + i + 1);
+                    i++;
+                }
+                    
+            }
         }
         sc.close();
     }
