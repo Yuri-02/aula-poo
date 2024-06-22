5e2fdcb80c331517f51c30eecc12870f8e41ced2
Yuri Albuquerque Silva <yuri.albuquerque.2@hotmail.com>
2024-06-21 21:16:04 -0300

    Exercício 2 resolvido

--- src/main/java/com/poo/introducao/Lista04.java
+++ src/main/java/com/poo/introducao/Lista04.java
@@ -16,4 +16,26 @@ public class Lista04 {
         System.out.println("Valor total das vendas do dia: R$ " + soma );
         System.out.println("--------------------------------------");
     }
+
+    void ex2(){
+        double[] precos = {2.50, 4.99, 9.98, 6.20, 15.49};
+        double maior=0;
+        double menor = 100000000;
+
+        for (int i=0; i<5; i++) {
+            if (precos[i] > maior){
+                maior = precos[i];
+            } 
+            if (precos[i] < menor){
+                menor = precos[i];
+            }
+        }
+
+        System.out.println("--------------------------------------");
+        System.out.println("Padaria do seu Zé");
+        System.out.println("--------------------------------------");
+        System.out.println("A venda de maior valor do dia foi de: R$ " + maior );
+        System.out.println("A venda de menor valor do dia foi de: R$ " + menor );
+        System.out.println("--------------------------------------");
+    }
 }
